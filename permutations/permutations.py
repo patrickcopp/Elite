67074c90063cdda13b5508259c1484104900c5f0
Patrick Copp <pcopp@uoguelph.ca>
2021-09-12 17:43:41 -0400

    Time: 70 ms (6.93%), Space: 14.5 MB (42.71%) - LeetHub

--- permutations/permutations.py
+++ permutations/permutations.py
@@ -1,4 +1,15 @@
-from itertools import permutations
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return permutations(nums)
+        res = []
+        def backtrack(current, nums):
+            if len(nums) == 0:
+                res.append(current)
+                return
+            for i in nums:
+                current.append(i)
+                tmp = nums[::]
+                tmp.remove(i)
+                backtrack(current[:], tmp)
+                current.pop()
+        backtrack([],nums)
+        return res
