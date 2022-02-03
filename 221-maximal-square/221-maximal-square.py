93d10f549e5b7dddbd57a8a75597e31b273a16d3
Patrick Copp <pcopp@uoguelph.ca>
2022-02-03 11:03:05 -0500

    Time: 958 ms (13.33%), Space: 16.6 MB (34.59%) - LeetHub

--- 221-maximal-square/221-maximal-square.py
+++ 221-maximal-square/221-maximal-square.py
@@ -8,7 +8,6 @@ class Solution:
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
                 if matrix[i][j] == 1 and i - 1 >= 0 and j - 1 >= 0:
-                    print(i,j)
                     matrix[i][j] = min(matrix[i - 1][j], matrix[i][j - 1],matrix[i - 1][j - 1]) + 1
                 high = max(matrix[i][j], high)
         return high **2
